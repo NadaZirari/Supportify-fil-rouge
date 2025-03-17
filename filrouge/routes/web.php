380e40f8bd7e548e2757238c61fabeaefd9b5db1
Nada Zirari <zirarinada20@gmail.com>
2025-03-17 11:44:55 +0000

    ajout routes of auth

--- filrouge/routes/web.php
+++ filrouge/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Afficher le formulaire d'inscription/connexion
+Route::get('/auth', function () {
+    return view('auth.register-login');
+})->name('auth.form');
