ac4cdd8316a7411c7577700a1e6464d8c0ebe83e
Nada Zirari <zirarinada20@gmail.com>
2025-04-07 14:27:27 +0100

    modifier les routes

--- filrouge/routes/web.php
+++ filrouge/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\TicketController;
+
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
 
@@ -11,7 +13,7 @@ Route::get('/', function () {
 });
 // Afficher le formulaire d'inscription/connexion
 Route::get('/auth', function () {
-    return view('auth.login');
+    return view('auth.register-login');
 })->name('auth.form');
 
 // Routes d'authentification
@@ -22,6 +24,10 @@ Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 // Routes d'enregistrement
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+
 
 // Routes de réinitialisation de mot de passe
 Route::get('/password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
