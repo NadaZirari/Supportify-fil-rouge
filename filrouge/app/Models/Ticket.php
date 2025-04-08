3e302a5c9e5e988ffe22dda42b6d4b747d94be19
Nada Zirari <zirarinada20@gmail.com>
2025-04-08 17:12:07 +0100

    ajouter le model du ticket avec ses champs necessaire

--- filrouge/app/Models/Ticket.php
+++ filrouge/app/Models/Ticket.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'description',
+        'priority',
+        'user_id',
+        'status', // Vous pourriez vouloir ajouter un statut par défaut
+    ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
