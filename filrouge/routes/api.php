55f04e9e3ac24361b7ef220e097909de42eaf717
Nada Zirari <zirarinada20@gmail.com>
2025-04-11 15:11:21 +0100

    ajouter les routes des functions qui existent dans ticket controller

--- filrouge/routes/api.php
+++ filrouge/routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\API\TicketController;
 use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,6 +12,11 @@ Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/tickets', [TicketController::class, 'index']);
+    Route::post('/tickets', [TicketController::class, 'store']);
+    Route::get('/tickets/{ticket}', [TicketController::class, 'show']);
+    Route::put('/tickets/{ticket}', [TicketController::class, 'update']);
+    Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
 });
