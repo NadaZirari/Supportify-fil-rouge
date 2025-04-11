6712070e29a82ac79f9636f1ec20865d1fcebaf0
Nada Zirari <zirarinada20@gmail.com>
2025-04-11 13:54:41 +0100

    creation de modem message avec leur relations

--- filrouge/app/Models/Message.php
+++ filrouge/app/Models/Message.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     //
+
+    public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function ticket()
+{
+    return $this->belongsTo(Ticket::class);
+}
+
 }
