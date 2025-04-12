d066746c8a6ce1a58bc0c62db32548c70f32e413
Nada Zirari <zirarinada20@gmail.com>
2025-04-12 18:20:49 +0100

    ajouter search bar

--- filrouge/resources/views/dashboardAgent.blade.php
+++ filrouge/resources/views/dashboardAgent.blade.php
@@ -85,6 +85,16 @@
             </nav>
         </div>
         
-        
+        <!-- Main Content -->
+        <div class="flex-1 overflow-auto p-6">
+            <!-- Search Bar -->
+            <div class="relative mb-6 w-[300px]">
+                <input type="text" placeholder="Search tickets..." class="w-full pl-10 pr-4 py-2 bg-dark-card border-none rounded-md text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400 absolute left-3 top-2.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </div>
+            
+          
 </body>
 </html>
