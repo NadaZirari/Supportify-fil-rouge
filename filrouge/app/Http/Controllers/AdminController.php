c5e45c623c472c56a9dea737673f6cf6d0db55e9
Nada Zirari <zirarinada20@gmail.com>
2025-04-12 18:56:20 +0100

    ajouter methode manage tocket

--- filrouge/app/Http/Controllers/AdminController.php
+++ filrouge/app/Http/Controllers/AdminController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    //
+    public function manageTickets()
+    {
+        // Récupère tous les tickets
+        $tickets = Ticket::all(); // ou utilise des filtres selon tes besoins
+
+        return view('admin.manage-tickets', compact('tickets'));
+    }
 }
