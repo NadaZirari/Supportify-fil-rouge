a1b8007591aa1592a3f63f0cbb63351d31e69754
Nada Zirari <zirarinada20@gmail.com>
2025-04-12 17:26:09 +0100

    ajouter index dans messagecontroller

--- filrouge/app/Http/Controllers/API/MessageController.php
+++ filrouge/app/Http/Controllers/API/MessageController.php
@@ -1,11 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\API;
-
+use App\Models\Message;
+use App\Models\Ticket;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function index(Ticket $ticket)
+    {
+        $this->authorizeAccess($ticket);
+        return $ticket->messages()->with('user')->orderBy('created_at')->get();
+    }
+
 }
