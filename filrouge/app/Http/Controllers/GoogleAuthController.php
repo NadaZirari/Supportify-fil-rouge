5573cb40fec9124637b6a4a12f3d14ef0e440918
Nada Zirari <zirarinada20@gmail.com>
2025-04-20 13:35:12 +0100

    ajouter model with migration of categorie

--- filrouge/app/Http/Controllers/GoogleAuthController.php
+++ filrouge/app/Http/Controllers/GoogleAuthController.php
@@ -52,7 +52,7 @@ class GoogleAuthController extends Controller
         }
 
         // Redirect the user to the dashboard or any other secure page
-        return redirect('/dashboard.dashboardUser');
+        return redirect('/user.dashboardUser');
     }
 }
 
