381c34e05415623c96e72d44e3c424a77fcdc359
Nada Zirari <zirarinada20@gmail.com>
2025-05-03 15:06:16 +0100

    modifier fonction index dans categorycontroller pour avoir la possibilité de count nbr ticket par categorue

--- filrouge/app/Http/Controllers/CategorieController.php
+++ filrouge/app/Http/Controllers/CategorieController.php
@@ -12,7 +12,7 @@ class CategorieController extends Controller
      */
     public function index()
     {
-        $categories = Categorie::latest()->paginate(10);
+        $categories = Categorie::withCount('tickets')->latest()->paginate(10);
         return view('categories.index', compact('categories'));
     }
 
